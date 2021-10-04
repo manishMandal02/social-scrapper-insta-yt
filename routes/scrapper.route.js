6ac38c110c01923e4d754896439968b95cb78876
manishMandal02 <manishmandalj@gmail.com>
2021-10-05 03:36:08 +0530

    test 02

--- routes/scrapper.route.js
+++ routes/scrapper.route.js
@@ -31,18 +31,27 @@ router.get('/insta', async (req, res) => {
   const socialName = req.query.socialName;
   const data = JSON.stringify({ account: socialName, posts_number: 10 });
   try {
-    await axios.post(
-      'https://api.luminati.io/dca/trigger_immediate?collector=c_kuc4ihta1b349jnh72',
-      {
-        data,
+    await axios({
+      method: 'post',
+      url: 'https://api.luminati.io/dca/trigger_immediate?collector=c_kuc4ihta1b349jnh72',
+      headers: {
+        Authorization: `Bearer ${process.env.BRIGHT_DATA_TOKEN}`,
+        'Content-Type': 'application/json',
       },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.BRIGHT_DATA_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+      data: data,
+    });
+    // await axios.post(
+    //   'https://api.luminati.io/dca/trigger_immediate?collector=c_kuc4ihta1b349jnh72',
+    //   {
+    //     data,
+    //   },
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.BRIGHT_DATA_TOKEN}`,
+    //       'Content-Type': 'application/json',
+    //     },
+    //   }
+    // );
     res.json({ status: 'success' });
   } catch (error) {
     console.log(error.response && error.response.data.message ? error.response.data.message : error.message);

118ee080b05a99b8dc95b28de8210e3a84f9f93f
manishMandal02 <manishmandalj@gmail.com>
2021-10-05 03:03:08 +0530

    minor fix

--- routes/scrapperWebHook.route.js
+++ routes/scrapperWebHook.route.js
@@ -3,16 +3,18 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/yt', async (req, res) => {
+router.post('/yt', async (req, res) => {
   const dataReceived = req.body;
   //   const data = JSON.stringify([{ url: channelLink, count_videos: 10 }]);
 
   res.json({ status: 'success', data: dataReceived });
 });
 
-router.get('/insta', async (req, res) => {
+router.post('/insta', async (req, res) => {
   const dataReceived = req.body;
   //   const data = JSON.stringify([{ url: channelLink, count_videos: 10 }]);
 
   res.json({ status: 'success', data: dataReceived });
 });
+
+module.exports = router;

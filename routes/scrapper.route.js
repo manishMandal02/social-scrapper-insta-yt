2c0b9faa8eb5bb65cf72316dcf1f96fdb5dbbb9a
manishMandal02 <manishmandalj@gmail.com>
2021-10-05 12:04:57 +0530

    testing webhook

--- routes/scrapper.route.js
+++ routes/scrapper.route.js
@@ -29,7 +29,8 @@ router.get('/yt', async (req, res) => {
 
 router.get('/insta', async (req, res) => {
   const socialName = req.query.socialName;
-  const data = JSON.stringify([{ account: socialName }]);
+  const data = JSON.stringify({ account: socialName, posts_number: '12' });
+  console.log(data);
   try {
     await axios({
       method: 'post',
@@ -40,19 +41,7 @@ router.get('/insta', async (req, res) => {
       },
       data: data,
     });
-    // await axios.post(
-    //   'https://api.luminati.io/dca/trigger_immediate?collector=c_kuc4ihta1b349jnh72',
-    //   {
-    //     data,
-    //   },
-    //   {
-    //     headers: {
-    //       Authorization: `Bearer ${process.env.BRIGHT_DATA_TOKEN}`,
-    //       'Content-Type': 'application/json',
-    //     },
-    //   }
-    // );
-    // res.json({ status: 'success' });
+    res.json({ status: 'success' });
   } catch (error) {
     console.log(error.response && error.response.data.message ? error.response.data.message : error.message);
     res.json({ status: 'error', data: error });
--- routes/scrapperWebHook.route.js
+++ routes/scrapperWebHook.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/yt', async (req, res) => {
   const dataReceived = req.body;
   //   const data = JSON.stringify([{ url: channelLink, count_videos: 10 }]);
-  
+
   console.log({ status: 'success', data: dataReceived });
 
   //   res.json({ status: 'success', data: dataReceived });
@@ -15,8 +15,8 @@ router.post('/yt', async (req, res) => {
 router.post('/insta', async (req, res) => {
   const dataReceived = req.body;
   //   const data = JSON.stringify([{ url: channelLink, count_videos: 10 }]);
-
-  console.log({ status: 'success', data: dataReceived });
+  console.log(req.body);
+  // console.log({ status: 'success', data: dataReceived });
 
   //   res.json({ status: 'success', data: dataReceived });
 });

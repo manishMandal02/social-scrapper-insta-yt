430b6f18cac089472acf516d54a9477ff76fb4f6
manishMandal02 <manishmandalj@gmail.com>
2021-10-05 13:08:18 +0530

    testing webhook-01

--- routes/scrapperWebHook.route.js
+++ routes/scrapperWebHook.route.js
@@ -5,20 +5,18 @@ const router = express.Router();
 
 router.post('/yt', async (req, res) => {
   const dataReceived = req.body;
-  //   const data = JSON.stringify([{ url: channelLink, count_videos: 10 }]);
 
   console.log({ status: 'success', data: dataReceived });
 
-  //   res.json({ status: 'success', data: dataReceived });
+  res.json({ status: 'success' });
 });
 
 router.post('/insta', async (req, res) => {
   const dataReceived = req.body;
-  //   const data = JSON.stringify([{ url: channelLink, count_videos: 10 }]);
   console.log(req.body);
   // console.log({ status: 'success', data: dataReceived });
 
-  //   res.json({ status: 'success', data: dataReceived });
+  res.json({ status: 'success' });
 });
 
 module.exports = router;
